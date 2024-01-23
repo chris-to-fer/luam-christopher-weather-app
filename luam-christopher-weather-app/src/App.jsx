a36913a1d7bc65163438f415e703dde5e6be8ad1
chris-to-fer <mail@christopherhaug.de>
2024-01-23 22:34:43 +0100

    location buttons and interval fix

--- luam-christopher-weather-app/src/App.jsx
+++ luam-christopher-weather-app/src/App.jsx
@@ -19,13 +19,20 @@ function App() {
   const [weather, setWeather] = useState("");
   const [condition, setCondition] = useState("");
   const [temperature, setTemperature] = useState("");
+  const [location, setLocation] = useLocalStorageState("location", {
+    defaultValue: "",
+  });
 
-  const url = "https://example-apis.vercel.app/api/weather";
-
+  function handleLocation(e) {
+    setLocation(e.target.value);
+  }
+  //let url = `https://example-apis.vercel.app/api/weather/${location}`;
   useEffect(() => {
     async function getWeather() {
       try {
-        const response = await fetch(url);
+        const response = await fetch(
+          `https://example-apis.vercel.app/api/weather/${location}`
+        );
         const data = await response.json();
         setWeather(data.isGoodWeather);
         setCondition(data.condition);
@@ -39,9 +46,9 @@ function App() {
     getWeather();
     const interval = setInterval(() => {
       getWeather();
-      return () => clearInterval(interval);
     }, 5000);
-  }, []);
+    return () => clearInterval(interval);
+  }, [location]);
 
   const isGoodWeather = weather;
 
@@ -68,13 +75,53 @@ function App() {
     setActivities([...activities, ...deleted.filter((a) => a.id === id)]);
     setDeleted(deleted.filter((a) => a.id !== id));
   }
-
+  console.log("loc ", location);
   console.log("acti ", activities);
   console.log("deleted ", deleted);
   return (
     <>
       <main>
+        <fieldset className="location-buttons">
+          <legend>Choose a location:</legend>
+          <input
+            onChange={handleLocation}
+            type="radio"
+            id="newyork"
+            name="loc"
+            value="arctic"
+            checked={location === "arctic"}
+          />
+          <label htmlFor="newyork">New York</label>
+          <input
+            onChange={handleLocation}
+            type="radio"
+            id="rio"
+            name="loc"
+            value="rainforest"
+            checked={location === "rainforest"}
+          />
+          <label htmlFor="rio">Rio</label>
+          <input
+            onChange={handleLocation}
+            type="radio"
+            id="tokio"
+            name="loc"
+            value="sahara"
+            checked={location === "sahara"}
+          />
+          <label htmlFor="tokio">Tokio</label>
+          <input
+            onChange={handleLocation}
+            type="radio"
+            id="berlin"
+            name="loc"
+            value="europe"
+            checked={location === "europe"}
+          />
+          <label htmlFor="berlin">Berlin</label>
+        </fieldset>
         <header>
+          {" "}
           <h1>{condition}</h1>
           <h1>{temperature}</h1>
         </header>
