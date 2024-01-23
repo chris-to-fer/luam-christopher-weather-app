6e9957949c87a5d5ac4bb3142c322fef75afa6e5
chris-to-fer <mail@christopherhaug.de>
2024-01-23 14:18:41 +0100

    waiting screen

--- luam-christopher-weather-app/src/List/List.jsx
+++ luam-christopher-weather-app/src/List/List.jsx
@@ -1,5 +1,6 @@
 import "./List.css";
 export default function List({
+  isLoading,
   activities,
   isGoodWeather,
   filteredActivities,
@@ -23,11 +24,15 @@ export default function List({
 
   return (
     <>
-      <h2>
-        {isGoodWeather === true
-          ? "The weather is awesome! Go outside and:"
-          : "Bad weather outside! Here's what you can do now:"}
-      </h2>
+      {isLoading === "start" ? (
+        ""
+      ) : (
+        <h2>
+          {isGoodWeather === true
+            ? "The weather is awesome! Go outside and:"
+            : "Bad weather outside! Here's what you can do now:"}
+        </h2>
+      )}
       <ul className="activity-container">{listItems}</ul>
     </>
   );
